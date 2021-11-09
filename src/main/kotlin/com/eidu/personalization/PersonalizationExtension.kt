f8a000431991f1bae5edb642c3d22569c455831a
tashonov <tashonov@gmail.com>
2021-11-09 12:34:52 +0100

    ✨ PersonalizationPlugin: add tensorflowModelInferenceRunner to interface - E112

--- src/main/kotlin/com/eidu/personalization/PersonalizationExtension.kt
+++ src/main/kotlin/com/eidu/personalization/PersonalizationExtension.kt
@@ -2,10 +2,15 @@ package com.eidu.personalization
 
 import com.eidu.content.reporting.RunWu
 import org.pf4j.ExtensionPoint
+import java.io.File
 
 interface PersonalizationPlugin : ExtensionPoint {
-    fun determineNextWu(input: PersonalizationInput): PersonalizationOutput
+    fun determineNextWu(
+        input: PersonalizationInput,
+        tensorflowModelInferenceRunner: ((modelFile: File, input: Any) -> (Any))? = null
+    ): PersonalizationOutput
 }
+
 data class PersonalizationInput(val progress: List<RunWu>, val contentIds: List<String>)
 
 data class WuProbability(val contentId: String, val probability: Float)

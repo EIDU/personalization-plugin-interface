4f8198ab78821eb837b825650edf5cb02e417e1a
tashonov <tashonov@gmail.com>
2021-11-09 12:36:23 +0100

    ♻️ PersonalizationPlugin: do not use WU term in interface - E112

--- src/main/kotlin/com/eidu/personalization/PersonalizationExtension.kt
+++ src/main/kotlin/com/eidu/personalization/PersonalizationExtension.kt
@@ -4,15 +4,17 @@ import com.eidu.content.reporting.RunWu
 import org.pf4j.ExtensionPoint
 import java.io.File
 
+typealias UnitOutcome = RunWu
+
 interface PersonalizationPlugin : ExtensionPoint {
-    fun determineNextWu(
+    fun determineNextContentIds(
         input: PersonalizationInput,
         tensorflowModelInferenceRunner: ((modelFile: File, input: Any) -> (Any))? = null
     ): PersonalizationOutput
 }
 
-data class PersonalizationInput(val progress: List<RunWu>, val contentIds: List<String>)
+data class PersonalizationInput(val progress: List<UnitOutcome>, val contentIds: List<String>)
 
-data class WuProbability(val contentId: String, val probability: Float)
+data class UnitProbability(val contentId: String, val probability: Float)
 
-data class PersonalizationOutput(val nextContentIds: List<String>, val probabilities: List<WuProbability>)
+data class PersonalizationOutput(val nextContentIds: List<String>, val probabilities: List<UnitProbability>)

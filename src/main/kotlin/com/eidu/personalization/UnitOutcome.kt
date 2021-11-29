dc3b5796edb0f2a8bd63a572d3edb73f95cca648
tashonov <tashonov@gmail.com>
2021-11-29 16:27:58 +0100

    ♻️ UnitOutcome: trim down details, add time - E112

--- src/main/kotlin/com/eidu/personalization/UnitOutcome.kt
+++ src/main/kotlin/com/eidu/personalization/UnitOutcome.kt
@@ -1,18 +1,12 @@
 package com.eidu.personalization
 
 import java.time.Duration
-import java.util.UUID
+import java.time.Instant
 
 data class UnitOutcome(
-    val runId: UUID,
-    val curriculumId: String,
-    val lessonId: String,
-    val contentId: String,
-    val sessionId: UUID,
-    val result: UnitResult,
+    val unitId: String,
+    val time: Instant,
     val duration: Duration,
-    val foregroundDuration: Duration? = null,
-    val sessionTimeAtStart: Duration? = null,
-    val foregroundSessionTimeAtStart: Duration? = null,
+    val result: UnitResult,
     val score: Float? = null
 )

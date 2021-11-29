5a947c3ba19a8f97c03851ec13a76c720d5a5a7a
tashonov <tashonov@gmail.com>
2021-11-29 16:15:43 +0100

    ♻️ runTensorflowInference: use resource path instead of a File - E112

--- src/main/kotlin/com/eidu/personalization/PersonalizationPlugin.kt
+++ src/main/kotlin/com/eidu/personalization/PersonalizationPlugin.kt
@@ -1,10 +1,8 @@
 package com.eidu.personalization
 
-import java.io.File
-
 interface PersonalizationPlugin {
     fun determineNextContentIds(
         input: PersonalizationInput,
-        runTensorflowInference: (modelFile: File, input: Any, output: Any) -> Unit
+        runTensorflowInference: (modelFileResourcePath: String, input: Any, output: Any) -> Unit
     ): PersonalizationOutput
 }

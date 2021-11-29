96c29000e7ca03c228195428993d0c13fea339aa
tashonov <tashonov@gmail.com>
2021-11-29 17:52:13 +0100

    ♻️ PersonalizationPlugin: rename fun - E112

--- src/main/kotlin/com/eidu/personalization/PersonalizationPlugin.kt
+++ src/main/kotlin/com/eidu/personalization/PersonalizationPlugin.kt
@@ -1,7 +1,7 @@
 package com.eidu.personalization
 
 interface PersonalizationPlugin {
-    fun determineNextContentIds(
+    fun determineNextUnits(
         input: PersonalizationInput,
         runTensorflowInference: (modelFileResourcePath: String, input: Any, output: Any) -> Unit
     ): PersonalizationOutput

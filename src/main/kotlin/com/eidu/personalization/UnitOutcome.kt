b74f261ac396a1bd2224dadc0218183cdc48760c
tashonov <tashonov@gmail.com>
2021-11-29 17:49:16 +0100

    ♻️ UnitOutcome: trim down details, remove time - E112

--- src/main/kotlin/com/eidu/personalization/UnitOutcome.kt
+++ src/main/kotlin/com/eidu/personalization/UnitOutcome.kt
@@ -1,11 +1,9 @@
 package com.eidu.personalization
 
 import java.time.Duration
-import java.time.Instant
 
 data class UnitOutcome(
     val unitId: String,
-    val time: Instant,
     val duration: Duration,
     val result: UnitResult,
     val score: Float? = null
